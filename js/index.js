a746712ea3ac76ef364a0bfe7e073c2de50246bf
esafraaz <esaf.raaz@gamil.com>
2022-02-04 17:29:44 +0600

    css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('i love you');
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data))
+}
+
+function displayPosts(posts){
+    const postsContainer = document.getElementById('ul_Id')
+    for(post of posts){
+        const li = document.createElement('li');
+        li.classList.add('postStyles');
+        li.innerHTML=`
+        <h2>${post.title}</h2>
+        <p>${post.body}<p>
+        `
+        postsContainer.appendChild(li);
+    }
+}
